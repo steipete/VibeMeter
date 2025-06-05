a859d8b1799807c7004be3ca013018151ff7cb5f
Peter Steinberger <steipete@gmail.com>
2025-06-05 01:02:51 +0100

    Prepare for fresh v1.0.0-beta.1 release (build 201)
    - Update build number to 201 (higher than existing 200)
    - Clear pre-release appcast for fresh start
    - Ready for new beta1 release

--- Project.swift
+++ Project.swift
@@ -17,7 +17,7 @@ let project = Project(
         base: [
             "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
             "COMBINE_HIDPI_IMAGES": true,
-            "CURRENT_PROJECT_VERSION": "100",
+            "CURRENT_PROJECT_VERSION": "201",
             "CODE_SIGN_STYLE": "Automatic",
             "DEVELOPMENT_TEAM": "Y5PE65HELJ",
             "ENABLE_HARDENED_RUNTIME": true,
